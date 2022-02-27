9f2f72a466ac18cc7cc395f9afa5355747365900
unknown <2020100538@pnu.edu.ru>
2022-02-27 21:57:43 +1000

    третье изменение

--- project.cpp
+++ project.cpp
@@ -4,7 +4,8 @@ using namespace std;
 int main()
 {
     cout << "Hello World!\n";
-    char hi[10];
+    char* hi = new char[10];
     cout << "Введите приветствие\n";
+    cin.getline(hi, 10);
 }
 
