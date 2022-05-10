71c2f87b3a5465ff6183d344b41b70a4f8c01fcc
unknown <2020100538@pnu.edu.ru>
2022-05-11 04:11:08 +1000

    Add doc

--- project.cpp
+++ project.cpp
@@ -1,7 +1,25 @@
 ﻿#include <iostream>
 #include <string.h>
-#include "function.h"
 using namespace std;
+/// Two numbers
+int a, b;
+/*!
+*    \brief multiply, multiplies two numders
+*    A function of type int, designed to multiply two numbers.
+*   @param a
+*   The first multiplier, type int.
+*   @param b
+*   The second multiplier, type int.
+*   @return
+*   Returns an int value, which is the result of multiplication
+*/
+int multiply(int a, int b) {
+    return a * b;
+}
+
+/*!
+* \brief the main function< the execution of the program begins with it
+*/
 int main()
 {
     cout << "Hello World!\n";
@@ -10,7 +28,6 @@ int main()
     cin.getline(hi, 10);
     cout << multiply(4, 5);
     cout << endl;
-    int a, b;
     cout << "Введите два числа: ";
     cin >> a;
     cin >> b;
